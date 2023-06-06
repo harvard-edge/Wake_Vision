023558df381f891ac38289933c29ef3ae06527c5
Emil Njor <emil.njor@gmail.com>
2023-06-06 14:14:13 +0200

    trimmed creation script

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -3,4 +3,4 @@ import tensorflow_datasets as tfds
 # Config
 DATA_DIR = "./dataset/"
 
-dataset = tfds.load("open_images_v4/200k", data_dir=DATA_DIR, with_info=True)
+dataset = tfds.load("open_images_v4/200k", batch_size=128, data_dir=DATA_DIR, with_info=True)
