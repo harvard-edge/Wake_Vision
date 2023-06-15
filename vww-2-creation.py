68751f4124a93648aff1a4fc0caadec4eee089f5
Emil Njor <emil.njor@gmail.com>
2023-06-15 16:06:58 +0200

    first try of labeling persons

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -1,6 +1,22 @@
+import tensorflow as tf
 import tensorflow_datasets as tfds
 
 # Config
 DATA_DIR = "./dataset/"
 
-dataset = tfds.load("open_images_v4/200k", batch_size=128, data_dir=DATA_DIR, with_info=True)
+ds, info = tfds.load(
+    "open_images_v4/200k",
+    batch_size=128,
+    data_dir=DATA_DIR,
+    with_info=True,
+    shuffle_files=True,
+)
+
+ds["train"].prefetch(tf.data.AUTOTUNE)  # Prefetch the data for improved performance
+
+
+def label_person(
+    bobjects, image, image_filename, objects, objects_trainable
+):  # These arguments are the features of one open images sample
+    if 68 in bobjects["Label"]:  # 68 is the integer label for person
+        return image, 1
