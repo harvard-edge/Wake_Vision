84e80a097845c3bc936eabe816bd79c1c5006d27
Emil Njor <emil.njor@gmail.com>
2023-06-16 15:19:13 +0200

    updated label_person function to work with dictionary structure of dataset

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -16,7 +16,10 @@ ds["train"].prefetch(tf.data.AUTOTUNE)  # Prefetch the data for improved perform
 
 
 def label_person(
-    bobjects, image, image_filename, objects, objects_trainable
+    ds_entry,
 ):  # These arguments are the features of one open images sample
-    if 68 in bobjects["Label"]:  # 68 is the integer label for person
-        return image, 1
+    if 68 in ds_entry["bobjects"]["Label"]:  # 68 is the integer label for person
+        return ds_entry["image"], 1
+
+
+example = ds["train"].map(label_person, num_parallel_calls=tf.data.AUTOTUNE)
