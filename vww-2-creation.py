65f0bb8e90c6f50bb64f0cb7730cc29285c84470
Emil Njor <emil.njor@gmail.com>
2023-06-16 17:23:38 +0200

    started using tf functions to compare tensor values

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -18,8 +18,10 @@ ds["train"].prefetch(tf.data.AUTOTUNE)  # Prefetch the data for improved perform
 def label_person(
     ds_entry,
 ):  # These arguments are the features of one open images sample
-    if 68 in ds_entry["bobjects"]["Label"]:  # 68 is the integer label for person
+    if tf.reduce_any(tf.equal(tf.constant(68, tf.int64) ,ds_entry["bobjects"]["label"])):  # 68 is the integer label for person
         return ds_entry["image"], 1
+    else:
+        return ds_entry["image"], 0
 
 
 example = ds["train"].map(label_person, num_parallel_calls=tf.data.AUTOTUNE)
