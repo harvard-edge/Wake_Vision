01bf99b19a87853a57b72455afb7cd66745534bc
Emil Njor <emil.njor@gmail.com>
2023-06-21 15:36:39 +0200

    finalized functionality to split dataset into person and non-person

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -4,11 +4,9 @@ import tensorflow_datasets as tfds
 # Config
 DATA_DIR = "./dataset/"
 
-ds, info = tfds.load(
+ds = tfds.load(
     "open_images_v4/200k",
-    batch_size=128,
     data_dir=DATA_DIR,
-    with_info=True,
     shuffle_files=True,
 )
 
@@ -17,11 +15,13 @@ ds["train"].prefetch(tf.data.AUTOTUNE)  # Prefetch the data for improved perform
 
 def label_person(
     ds_entry,
-):  # These arguments are the features of one open images sample
-    if tf.reduce_any(tf.equal(tf.constant(68, tf.int64) ,ds_entry["bobjects"]["label"])):  # 68 is the integer label for person
-        return ds_entry["image"], 1
+):
+    if tf.reduce_any(
+        tf.equal(tf.constant(1208, tf.int64), ds_entry["objects"]["label"])
+    ):  # 1208 is the integer value for the image level label for person
+        ds_entry["person"] = 1
     else:
-        return ds_entry["image"], 0
+        ds_entry["person"] = 0
 
 
-example = ds["train"].map(label_person, num_parallel_calls=tf.data.AUTOTUNE)
+ds["train"] = ds["train"].map(label_person, num_parallel_calls=tf.data.AUTOTUNE)
