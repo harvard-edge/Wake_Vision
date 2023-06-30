14bf9a32b68a537294322431d35c3d766413189e
Emil Njor <emil.njor@gmail.com>
2023-06-30 14:33:02 +0200

    initial training script for mobilenetv1

--- vww-2-creation.py
+++ vww-2-creation.py
@@ -33,6 +33,10 @@ def non_person_filter(ds_entry):
     return tf.equal(ds_entry["person"], 0)
 
 
+def crop_images(ds_entry):
+    return tf.image.resize_with_crop_or_pad(ds_entry, 224, 224)
+
+
 ds["train"] = ds["train"].map(label_person, num_parallel_calls=tf.data.AUTOTUNE)
 
 person_ds = ds["train"].filter(person_filter)
@@ -45,3 +49,45 @@ ds["train"] = person_ds.concatenate(non_person_ds)
 
 # Shuffle the entire dataset again. This may require too much memory.
 ds["train"] = ds["train"].shuffle(2 * COUNT_PERSON_SAMPLES)
+
+## Start of inference testing part
+
+# Optimizer
+optimizer = tf.keras.optimizers.RMSprop(
+    learning_rate=0.045, momentum=0.9, weight_decay=0.9
+)
+
+# Loss
+loss = tf.keras.losses.SparseCategoricalCrossentropy()
+
+# Decay & Momentum: 0.9
+# Batch size: 96
+# Learning rate: 0.045 initially decaying by .98 per epoch.
+
+# Conv batch norm decay: 0.99
+# Trained with quantization aware training 10^-5 learning rate and 0.9 decay
+
+# Try out training and inference with mobilenet v1
+# Parameters given to models are the same as for the models used in the visual wake words paper
+
+mobilenetv1_train = ds["train"].map(crop_images, num_parallel_calls=tf.data.AUTOTUNE)
+mobilenetv1_train = tf.keras.applications.mobilenet.preprocess_input(mobilenetv1_train)
+
+mobilenetv1_train = mobilenetv1_train.batch(96).prefetch(tf.data.AUTOTUNE)
+
+mobilenetv1 = tf.keras.applications.MobileNet(
+    depth_multiplier=0.25, weights=None, classes=2
+)
+
+mobilenetv1.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
+
+mobilenetv1.fit(ds["train"]["image"], ds["train"]["person"], epochs=10)
+
+# Also try out Mobilenet v2
+# mobilenetv2_train = ds["train"].map(crop_images, num_parallel_calls=tf.data.AUTOTUNE)
+# mobilenetv2_train = tf.keras.applications.mobilenet_v2.preprocess_input(
+#    mobilenetv2_train
+# )
+#
+## The depth multiplier should be set to 0.5 but this does not seem to be supported in this api.
+# mobilenetv2 = tf.keras.applications.MobileNetV2(weights=None, classes=2)
