41bf59f36ad007bb8fce9f285da1b2e2e8f269d4
Emil Njor <emil.njor@gmail.com>
2023-11-30 18:23:27 +0000

    updated renaming script

--- bootstrap_open_images.py
+++ bootstrap_open_images.py
@@ -5,20 +5,13 @@ import os
 
 SPLIT = "validation"
 
+source_bucket = storage.Client().bucket("wake-vision-storage")
 
-for blob in storage.Client().list_blobs(
-    "wake-vision-storage",
+for blob in source_bucket.list_blobs(
     prefix=f"tensorflow_datasets/downloads/manual/{SPLIT}/",
     match_glob="**.jpg",
 ):
-    # Download the image.
-    image_fobj = open("temp.jpg", "wb")
-    blob.download_to_file(image_fobj)
-    image_fobj = open("temp.jpg", "rb")
-    # Upload the image.
-    new_blob = storage.Blob(
+    source_bucket.rename_blob(
+        blob,
         f"tensorflow_datasets/downloads/manual/{SPLIT}/{os.path.basename(blob.name)}",
-        blob.bucket,
     )
-    new_blob.upload_from_file(image_fobj)
-    blob.delete()
