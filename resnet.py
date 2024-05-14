f092d15863acab587809a76df2a1f1898e478e77
Colby Banbury <cbanbury@g.harvard.edu>
2024-05-14 16:12:33 +0000

    adding resnet 18 and 34

--- resnet.py
+++ resnet.py
@@ -5,7 +5,7 @@ from keras.layers import Conv2D, AveragePooling2D, MaxPooling2D
 from keras.regularizers import l2
 import numpy as np
 
-
+#pulled directly from the MLPerf Tiny benchmark suite
 def resnet_mlperf(input_shape, num_classes):
     # Resnet parameters
 
@@ -160,3 +160,54 @@ def resnet_mlperf(input_shape, num_classes):
     # Instantiate model.
     model = Model(inputs=inputs, outputs=outputs)
     return model
+
+
+def resnet_builder(input_shape, num_classes, layers=[2,2,2,2]):
+    
+    inputs = Input(shape=input_shape)
+    init_filters = 64
+
+    # resnet bottom
+    x = keras.layers.BatchNormalization(name='bn_data')(inputs)
+    x = keras.layers.ZeroPadding2D(padding=(3, 3))(x)
+    x = keras.layers.Conv2D(init_filters, (7, 7), strides=(2, 2), name='conv0')(x)
+    x = keras.layers.BatchNormalization(name='bn0')(x)
+    x = keras.layers.Activation('relu', name='relu0')(x)
+    x = keras.layers.ZeroPadding2D(padding=(1, 1))(x)
+    x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='valid', name='pooling0')(x)
+
+    # resnet body
+    for stage, rep in enumerate(layers):
+        for block in range(rep):
+
+            filters = init_filters * (2 ** stage)
+
+            # first block of first stage without strides because we have maxpooling before
+            if block == 0 and stage == 0:
+                x = ResidualBlock(filters, stage, block, strides=(1, 1),
+                                  cut='post', attention=Attention)(x)
+
+            elif block == 0:
+                x = ResidualBlock(filters, stage, block, strides=(2, 2),
+                                  cut='post', attention=Attention)(x)
+
+            else:
+                x = ResidualBlock(filters, stage, block, strides=(1, 1),
+                                  cut='pre', attention=Attention)(x)
+
+    x = keras.layers.BatchNormalization(name='bn1')(x)
+    x = keras.layers.Activation('relu', name='relu1')(x)
+
+    # resnet top
+    x = keras.layers.GlobalAveragePooling2D(name='pool1')(x)
+    x = keras.layers.Dense(num_classes, name='fc1')(x)
+    x = keras.layers.Activation('softmax', name='softmax')(x)
+    
+    model = Model(inputs=inputs, outputs=x)
+    return model
+
+def resnet18(input_shape, num_classes):
+    return resnet_builder(input_shape, num_classes, layers=[2,2,2,2])
+
+def resnet34(input_shape, num_classes):
+    return resnet_builder(input_shape, num_classes, layers=[3, 4, 6, 3])
