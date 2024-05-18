95e567281ce19dbe7f7be8f826193eb2c6400ef6
Colby Banbury <cbanbury@g.harvard.edu>
2024-05-18 21:45:10 +0000

    fix minor bug in tfds val and add new sweep

--- sweep_data_size_and_error_rate.sh
+++ sweep_data_size_and_error_rate.sh
@@ -6,13 +6,16 @@ TRAINING_SCRIPT="train.py"
 # Define a list of learning rates to sweep over
 Dataset_size=(10 25 50)
 Error_rate=(0.0 0.095 0.269)
-MODEL="resnet_mlperf"
+MODEL="resnet18"
 
-# Loop through each learning rate
-for ds_s in "${Dataset_size[@]}"; do
-  for er in "${Error_rate[@]}"; do
-    # Run the training script
-    python "$TRAINING_SCRIPT" -m="$MODEL" -e="$er" -p="$ds_s" -n="${MODEL}_dsize_${ds_s}_error_${er}"
+RUNS=(1)
+
+for run in "${RUNS[@]}"; do
+  for ds_s in "${Dataset_size[@]}"; do
+    for er in "${Error_rate[@]}"; do
+      # Run the training script
+      python "$TRAINING_SCRIPT" -m="$MODEL" -e="$er" -p="$ds_s" -n="${MODEL}_dsize_${ds_s}_error_${er}_run_${run}_normalsteps"
+    done
   done
 done
 
