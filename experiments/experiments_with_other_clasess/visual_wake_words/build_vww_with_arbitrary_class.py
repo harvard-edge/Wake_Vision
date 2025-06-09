df66f62a45c6be8183022f78ad9c19959f171c38
AndreaMattiaGaravagno <andrexroma.ag@gmail.com>
2025-06-09 19:39:53 -0400

    added code for experiments with other classes

---
+++ experiments/experiments_with_other_clasess/visual_wake_words/build_vww_with_arbitrary_class.py
@@ -0,0 +1,180 @@
+from urllib.request import urlretrieve
+from pycocotools.coco import COCO
+from pathlib import Path
+import subprocess
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Visual Wake Words binary dataset builder")
+
+parser.add_argument("target", type=str,
+    help="Target class (e.g. bird).\n For available classes see: https://cocodataset.org/#explore")
+
+args = parser.parse_args()
+
+target_class = args.target
+
+print("\nPlease be patient, it will take some time...\n")
+
+annotations_filename = 'annotations_trainval2014.zip'
+
+if not Path(annotations_filename).exists() :
+    urlretrieve('http://images.cocodataset.org/annotations/annotations_trainval2014.zip', filename=annotations_filename)
+
+train_filename = 'train2014.zip'
+
+if not Path(train_filename).exists() :
+    urlretrieve('http://images.cocodataset.org/zips/train2014.zip', filename=train_filename)
+    
+val_filename = 'val2014.zip'
+
+if not Path(train_filename).exists() :
+    urlretrieve('http://images.cocodataset.org/zips/val2014.zip', filename=val_filename)
+
+os.system('unzip annotations_trainval2014 -d data')
+#os.system('rm annotations_trainval2014.zip')
+os.system('unzip train2014 -d data')
+#os.system('rm train2014.zip')
+os.system('unzip val2014 -d data')
+#os.system('rm val2014.zip')
+
+os.system(f"mkdir -p {target_class}/1")
+os.system(f"mkdir -p {target_class}/0")
+
+#The label 1 is assigned as long as it has at least one bounding box
+#corresponding to the object of interest (e.g. person) with the box area greater
+#than 0.5% of the image area. - from https://arxiv.org/pdf/1906.05721.pdf
+#The label 0 is assigned otherwise
+#1 is target
+def filter_ids(ids, foreground_class_of_interest_id=1, small_object_area_threshold=0.005) :
+  filtered_ids = list()
+
+  for id in ids :
+    imAnn = coco.loadImgs(id)[0]
+    Anns = coco.loadAnns(coco.getAnnIds(id))
+
+    image_area = imAnn['height'] * imAnn['width']
+
+    for annotation in Anns:
+      normalized_object_area = annotation['area'] / image_area
+      category_id = int(annotation['category_id'])
+      # Filter valid bounding boxes
+      if category_id == foreground_class_of_interest_id and \
+          normalized_object_area > small_object_area_threshold:
+        #if one is found, then the image is good
+        #so you can append its index to the filtered ones
+        filtered_ids.append(id)
+        #and stop the search
+        break
+  discarded_ids = list(set(ids) - set(filtered_ids))
+
+  return filtered_ids, discarded_ids
+
+#Extracting images from COCO validation set 2014
+pathToInstances = './data/annotations/instances_val2014.json'
+coco = COCO(pathToInstances)
+
+#get all images ids
+allIds = coco.getImgIds()
+img_count_val = len(allIds)
+targetIds = coco.getImgIds(catIds=coco.getCatIds([target_class]))
+backgroundIds = list(set(allIds) - set(targetIds))
+
+targetIds, discardedIds = filter_ids(targetIds)
+backgroundIds = list(set(backgroundIds) | set(discardedIds))
+
+#load images metadata
+targetImages = coco.loadImgs(targetIds)
+backgroundImages = coco.loadImgs(backgroundIds)
+
+#write images' names onto a textual file
+sourceFile = open('target_images.txt', 'w')
+for im in targetImages :
+  print('./data/val2014/' + im['file_name'], file = sourceFile)
+sourceFile.close()
+
+sourceFile = open('background_images.txt', 'w')
+for im in backgroundImages :
+  print('./data/val2014/' + im['file_name'], file = sourceFile)
+sourceFile.close()
+
+#move images from COCO to the new dataset using the textual files
+print('Moving COCO 2014 validation images...')
+os.system(f"cat target_images.txt | xargs -I % mv % {target_class}/1")
+os.system(f"cat background_images.txt | xargs -I % mv % {target_class}/0")
+
+os.system('rm target_images.txt')
+os.system('rm background_images.txt')
+
+#Extracting images from COCO training set 2014
+pathToInstances = './data/annotations/instances_train2014.json'
+coco = COCO(pathToInstances)
+
+#get all images ids
+allIds = coco.getImgIds()
+img_count_val = len(allIds)
+targetIds = coco.getImgIds(catIds=coco.getCatIds([target_class]))
+backgroundIds = list(set(allIds) - set(targetIds))
+
+targetIds, discardedIds = filter_ids(targetIds)
+backgroundIds = list(set(backgroundIds) | set(discardedIds))
+
+#load images metadata
+targetImages = coco.loadImgs(targetIds)
+backgroundImages = coco.loadImgs(backgroundIds)
+
+#write images' names onto a textual file
+sourceFile = open('target_images.txt', 'w')
+for im in targetImages :
+  print('./data/train2014/' + im['file_name'], file = sourceFile)
+sourceFile.close()
+
+sourceFile = open('background_images.txt', 'w')
+for im in backgroundImages :
+  print('./data/train2014/' + im['file_name'], file = sourceFile)
+sourceFile.close()
+
+#move images from COCO to the new dataset using the textual files
+print('Moving COCO 2014 training images...')
+os.system(f"cat target_images.txt | xargs -I % mv % {target_class}/1")
+os.system(f"cat background_images.txt | xargs -I % mv % {target_class}/0")
+
+os.system('rm target_images.txt')
+os.system('rm background_images.txt')
+
+os.system('rm -rf data')
+
+n_of_targetImages = len(list((Path(target_class) / '1').glob('*.jpg')))
+n_of_backgroundImages = len(list((Path(target_class) / '0').glob('*.jpg')))
+
+print(f"Number of target images: {n_of_targetImages}")
+print(f"Number of background images: {n_of_backgroundImages}")
+
+print("Balancing dataset")
+
+delta = n_of_targetImages - n_of_backgroundImages
+
+images_to_remove_count = abs(delta)
+
+print(f"Number of images to remove: {images_to_remove_count}")
+
+if delta > 0 :
+    #remove images from target class
+    for image in (Path(target_class) / '1').glob('*.jpg') :
+        image.unlink()
+        images_to_remove_count = images_to_remove_count - 1
+        if images_to_remove_count <= 0 :
+            break
+elif delta < 0 :
+    #remove images from background class
+    for image in (Path(target_class) / '0').glob('*.jpg') :
+        image.unlink()
+        images_to_remove_count = images_to_remove_count - 1
+        if images_to_remove_count <= 0 :
+            break
+
+print(f"Number of target images: {len(list((Path(target_class) / '1').glob('*.jpg')))}")
+print(f"Number of background images: {len(list((Path(target_class) / '0').glob('*.jpg')))}")
+
+path = os.path.abspath(f"./{target_class}/")
+print(f"\nDataset saved in: {path}\n")
---
+++ experiments/experiments_with_other_clasess/wake_vision/build_wake_vision_with_other_targets.py
@@ -0,0 +1,234 @@
+from urllib.request import urlretrieve
+from pathlib import Path
+import subprocess
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description="Wake Vision binary dataset builder")
+
+parser.add_argument("target", type=str,
+    help="Target class (e.g. Bird).\n For available classes see: https://storage.googleapis.com/openimages/v7/oidv7-class-descriptions-boxable.csv")
+parser.add_argument(
+    '--num_processes',
+    type=int,
+    default=8,
+    help='Number of parallel processes to use (default is 8).')
+args = parser.parse_args()
+
+target = args.target
+num_processes = args.num_processes
+
+confidence_threshold = 0.7
+
+dataset_name = f"Wake_Vision_{target}"
+path_to_dataset = Path(dataset_name)
+path_to_dataset.mkdir()
+
+print("The entire process requires time. Please be patient.")
+print("Downloading required files...")
+
+#download required files from OpenImageV7
+class_descriptions_boxable_filename = 'oidv7-class-descriptions-boxable.csv'
+
+if not Path(class_descriptions_boxable_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v7/oidv7-class-descriptions-boxable.csv', filename=class_descriptions_boxable_filename)
+
+test_annotations_filename = 'test-annotations-bbox.csv'
+
+if not Path(test_annotations_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v5/test-annotations-bbox.csv', filename=test_annotations_filename)
+    
+validation_annotations_filename = 'validation-annotations-bbox.csv'
+
+if not Path(validation_annotations_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv', filename=validation_annotations_filename)
+
+train_annotations_filename = 'oidv6-train-annotations-bbox.csv'
+
+if not Path(train_annotations_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-bbox.csv', filename=train_annotations_filename)
+
+train_large_machine_annotations_filename = 'oidv7-train-annotations-machine-imagelabels.csv'
+
+if not Path(train_large_machine_annotations_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v7/oidv7-train-annotations-machine-imagelabels.csv', filename=train_large_machine_annotations_filename)
+    
+train_large_human_annotations_filename = 'oidv7-train-annotations-human-imagelabels.csv'
+
+if not Path(train_large_human_annotations_filename).exists() :
+    urlretrieve('https://storage.googleapis.com/openimages/v7/oidv7-train-annotations-human-imagelabels.csv', filename=train_large_human_annotations_filename)
+
+downloader_filename = 'downloader.py'
+
+label_name = None
+
+#check if target exists and fetch corresponding label code
+with open(class_descriptions_boxable_filename, newline='') as csvfile :
+    reader = csv.DictReader(csvfile)
+    for row in reader :
+        if row['DisplayName'] == target :
+            label_name = row['LabelName']
+            break
+
+if label_name == None :
+    print("Target not found.")
+    exit()
+
+splits = ('train', 'validation', 'test')
+annotations_files = (train_annotations_filename, validation_annotations_filename, test_annotations_filename)
+
+print(f"Target: {target}")
+
+for split, annotations_filename in zip(splits, annotations_files) :
+    target_images = set()
+    background_images = set()
+    
+    print(f"Preparing {split} split...")
+
+    with open(annotations_filename, newline='') as csvfile :
+        reader = csv.DictReader(csvfile)
+        for row in reader :
+            if row['LabelName'] == label_name :
+                if row['IsTruncated'] == '0' and row['IsDepiction'] == '0' :
+                    area = (float(row['XMax']) - float(row['XMin'])) * (float(row['YMax']) - float(row['YMin']))
+                    if area > 0.05 :
+                        target_images.add(row['ImageID']) 
+            else :
+                background_images.add(row['ImageID'])
+                
+    print(len(target_images))
+    print(len(background_images))
+    
+    #balance 
+    n_targets = len(target_images)
+    n_backgrounds = len(background_images)
+    
+    delta = n_targets - n_backgrounds
+    
+    if delta > 0 :
+        n_targets = n_backgrounds
+    else :
+        n_backgrounds = n_targets
+    
+    #sort entries for reproducibility
+    target_images = list(target_images)
+    background_images = list(background_images)    
+    
+    target_images.sort()
+    background_images.sort()
+    
+    #balance
+    target_images = target_images[:n_targets]
+    background_images = background_images[:n_backgrounds]
+    
+    path_to_target_images_list = Path('target_images.txt')
+    path_to_background_images_list = Path('background_images.txt')
+    
+    with open(path_to_target_images_list, 'w') as f:
+       for image in target_images :
+           print(f"{split}/{image}", file=f)
+    
+    with open(path_to_background_images_list, 'w') as f:
+       for image in background_images :
+           print(f"{split}/{image}", file=f)
+    
+    path_to_target_images = path_to_dataset / split / target
+    path_to_background_images = path_to_dataset / split / 'background'
+
+    path_to_target_images.mkdir(parents=True, exist_ok=True)
+    path_to_target_images.mkdir(parents=True, exist_ok=True)
+
+    subprocess.run(["python3", downloader_filename, str(path_to_target_images_list), f"--download_folder={str(path_to_target_images)}", f"--num_processes={num_processes}"])
+    subprocess.run(["python3", downloader_filename, str(path_to_background_images_list), f"--download_folder={str(path_to_background_images)}", f"--num_processes={num_processes}"])
+
+    path_to_target_images_list.unlink()
+    path_to_background_images_list.unlink()
+    
+    if split == 'train' :
+        train_quality_images = set(target_images) | set(background_images)
+        
+(path_to_dataset / 'train').rename(Path(path_to_dataset / 'train_quality'))
+
+#train large split
+print(f"Preparing train_large split...")
+split = 'train'
+
+target_images = set()
+background_images = set()
+
+with open(train_large_human_annotations_filename, newline='') as csvfile :
+    reader = csv.DictReader(csvfile)
+    for row in reader :
+        if row['LabelName'] == label_name :
+            if row['Confidence'] == '1' :
+                target_images.add(row['ImageID'])
+            elif row['Confidence'] == '0' :
+                background_images.add(row['ImageID'])
+        else :
+            background_images.add(row['ImageID'])
+
+with open(train_large_machine_annotations_filename, newline='') as csvfile :
+    reader = csv.DictReader(csvfile)
+    for row in reader :
+        if row['LabelName'] == label_name :
+            if float(row['Confidence']) >= confidence_threshold :
+                if not row['ImageID'] in background_images :
+                    target_images.add(row['ImageID'])
+            else :
+                background_images.add(row['ImageID'])
+
+#remove images already present in train_quality
+target_images = target_images - train_quality_images
+background_images = background_images - train_quality_images
+
+print(len(target_images))
+print(len(background_images))
+    
+#balance 
+n_targets = len(target_images)
+n_backgrounds = len(background_images)
+    
+delta = n_targets - n_backgrounds
+    
+if delta > 0 :
+    n_targets = n_backgrounds
+else :
+    n_backgrounds = n_targets
+    
+#sort entries for reproducibility
+target_images = list(target_images)
+background_images = list(background_images)    
+    
+target_images.sort()
+background_images.sort()
+    
+#balance
+target_images = target_images[:n_targets]
+background_images = background_images[:n_backgrounds]
+    
+path_to_target_images_list = Path('target_images.txt')
+path_to_background_images_list = Path('background_images.txt')
+
+with open(path_to_target_images_list, 'w') as f:
+   for image in target_images :
+       print(f"{split}/{image}", file=f)
+    
+with open(path_to_background_images_list, 'w') as f:
+   for image in background_images :
+       print(f"{split}/{image}", file=f)
+
+path_to_target_images = path_to_dataset / split / target
+path_to_background_images = path_to_dataset / split / 'background'
+
+path_to_target_images.mkdir(parents=True, exist_ok=True)
+path_to_target_images.mkdir(parents=True, exist_ok=True)
+
+subprocess.run(["python3", downloader_filename, str(path_to_target_images_list), f"--download_folder={str(path_to_target_images)}", f"--num_processes={num_processes}"])
+subprocess.run(["python3", downloader_filename, str(path_to_background_images_list), f"--download_folder={str(path_to_background_images)}", f"--num_processes={num_processes}"])
+
+path_to_target_images_list.unlink()
+path_to_background_images_list.unlink()
+
+(path_to_dataset / 'train').rename(Path(path_to_dataset / 'train_large'))
+
+print(f"Dataset saved in {path_to_dataset.resolve()}")
